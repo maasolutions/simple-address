48d5ed57595ebbefd922fa8d8c9f48c7d984718e
Marc-André Appel <marcandre.appel@gmail.com>
2024-07-23 10:42:34 +0200

    fix: fixes the column name

--- src/Models/Address.php
+++ src/Models/Address.php
@@ -6,11 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 /**
- * @property string $address
- * @property string $address_additional_info
- * @property string $city_name
+ * @property string $street_address
+ * @property string $street_address_complement
+ * @property string $city
+ * @property string $state_province
  * @property string $zip_code
  * @property string $country
+ * @property string $latitude
+ * @property string $longitude
  */
 class Address extends Model
 {
@@ -18,7 +21,7 @@ class Address extends Model
     protected $fillable = [
         'street_address',
         'street_address_complement',
-        'city_name',
+        'city',
         'state_province',
         'zip_code',
         'country',
